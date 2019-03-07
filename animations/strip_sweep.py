5d1892c0e998027b92f7a005281300bf21b12ffc
Jim Conner <jimconner@users.noreply.github.com>
2019-03-07 17:42:24 +0000

    Cycle through the different white strips.

--- animations/strip_sweep.py
+++ animations/strip_sweep.py
@@ -7,18 +7,21 @@ from numpy import array,full
 class strip_sweep():
     def __init__(self,max_led):
         self.sweep_pos=0
+        self.colpos=0
         self.max_led=max_led
 
     def emit_row(self):
         try:
+            if self.sweep_pos == 0:
+                self.colpos=(self.colpos+1) %4 
             self.sweep_pos = (self.sweep_pos +1) % (self.max_led-1)
             row_arr=full((self.max_led,4),0)
             brt=(self.sweep_pos % 30)*8
             brtlo=240-(self.sweep_pos % 30)*8 
             brthi=(self.sweep_pos % 30) *8
-            row_arr[int(((self.sweep_pos/30)-1)*30 % self.max_led)]=[0,brtlo,0,0]
-            row_arr[int(self.sweep_pos/30)*30]=[0,240,0,0]
-            row_arr[int(((self.sweep_pos/30)+1)*30 % self.max_led)]=[0,brthi,0,0]
+            row_arr[int(((self.sweep_pos/30)-1)*30 % self.max_led)][self.colpos]=brtlo
+            row_arr[int(self.sweep_pos/30)*30][self.colpos]=240
+            row_arr[int(((self.sweep_pos/30)+1)*30 % self.max_led)][self.colpos]=brthi
             return row_arr
         except Exception as err:
             print(err)
