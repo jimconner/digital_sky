1e9a963b968692b42d75f76dacc752510055e9ef
Jim Conner <jimconner@users.noreply.github.com>
2019-12-23 20:05:56 +0000

    Set only the built in strips, not the extra 12 PWM outputs.

--- plugins/set_strips.py
+++ plugins/set_strips.py
@@ -10,9 +10,10 @@ class strip_animation():
 
     def emit_row(self):
         try:
-            row_arr=full((self.datastore.LED_COUNT,4),[int(self.datastore.strip_vals[0]), int(self.datastore.strip_vals[1]), int(self.datastore.strip_vals[2]), int(self.datastore.strip_vals[3])])
+            row_arr=full((self.datastore.LED_COUNT,4),0)
+            for lamp in range(0, int(self.datastore.LED_COUNT / self.datastore.LAMP_LENGTH)):
+                row_arr[ (self.datastore.LAMP_LENGTH * lamp) ]=[int(self.datastore.strip_vals[0]), int(self.datastore.strip_vals[1]), int(self.datastore.strip_vals[2]), int(self.datastore.strip_vals[3])]
             return row_arr
         except Exception as err:
             print(err)
             traceback.print_exc(file=sys.stdout)
-
