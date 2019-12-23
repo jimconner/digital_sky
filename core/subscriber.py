ea3be815116c7ea16aedcc5502c600b667a7f1a4
Jim Conner <jimconner@users.noreply.github.com>
2019-12-23 18:53:54 +0000

    Support loading/saving of presets
    Cheap and lazy.. use pickle to save/load the datastore values.
    Also, change of language used around power on/off states and auto-power-on when sensible.

--- core/subscriber.py
+++ core/subscriber.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, json, pickle
 
 from twisted.internet.defer       import inlineCallbacks, DeferredList
 from twisted.internet             import reactor
@@ -87,31 +87,53 @@ class MQTTService(ClientService):
             print(payload)
         if topic == 'hermes/hotword/default/detected':
             print("Snips is listening")
-            self.datastore.add_animation('pulse')
+            if self.datastore.power == 1:
+                self.datastore.add_animation('pulse')
         if topic == 'hermes/intent/jimconner:natural':
             print("Natural White")
+            if self.datastore.power == 0:
+                self.datastore.set_power(1)
             self.datastore.strip_vals[0]=int(payload['slots'][0]['value']['value']*2.55)
         if topic == 'hermes/intent/jimconner:daylight':
             self.log.info("Daylight White")
+            if self.datastore.power == 0:
+                self.datastore.set_power(1)
             self.datastore.strip_vals[1]=int(payload['slots'][0]['value']['value']*2.55)
         if topic == 'hermes/intent/jimconner:blue':
             self.log.info("Ice Blue")
+            if self.datastore.power == 0:
+                self.datastore.set_power(1)
             self.datastore.strip_vals[2]=int(payload['slots'][0]['value']['value']*2.55)
         if topic == 'hermes/intent/jimconner:warm':
             self.log.info("Warm White")
+            if self.datastore.power == 0:
+                self.datastore.set_power(1)
             self.datastore.strip_vals[3]=int(payload['slots'][0]['value']['value']*2.55)
-        if topic == 'hermes/intent/jimconner:lights_off':
-            self.log.info("Lights Off")
-            self.datastore.strip_vals=[0,0,0,0]
-            self.datastore.animations=[]
-            self.datastore.strip_animations=[]
-            self.datastore.add_animation("set_strips")
+        if topic == 'hermes/intent/jimconner:load':
+            self.log.info("Load Preset")
+            all_the_data = pickle.load(open('presets/'+payload['slots'][0]['rawValue'], 'rb'))
+            self.datastore.animations = all_the_data['animations']
+            self.datastore.strip_animations = all_the_data['strip_animations']
+            self.datastore.filters = all_the_data['filters']
+            self.datastore.strip_vals = all_the_data['strip_vals']
+            self.datastore.set_power(all_the_data['power'])
+        if topic == 'hermes/intent/jimconner:save':
+            self.log.info("Save Preset")
+            print("Got ", payload['slots'][0]['rawValue'])
+            all_the_data={ 'animations' : self.datastore.animations,
+                            'strip_animations' : self.datastore.strip_animations,
+                            'filters' : self.datastore.filters,
+                            'strip_vals' : self.datastore.strip_vals,
+                            'power' : self.datastore.power }
+            pickle.dump(all_the_data, open('presets/'+payload['slots'][0]['rawValue'], 'wb'))
         if topic == 'hermes/intent/jimconner:power':
             self.log.info("Power Supply Command")
             if len(payload['slots']) == 1:
                 self.datastore.set_power(payload['slots'][0]['value']['value'])
         if topic == 'hermes/intent/jimconner:add':
             self.log.info("Add a thing")
+            if self.datastore.power == 0:
+                self.datastore.set_power(1)
             if len(payload['slots']) == 1:
                 self.datastore.add_animation(payload['slots'][0]['value']['value'])
             elif len(payload['slots']) == 2:
@@ -121,8 +143,15 @@ class MQTTService(ClientService):
             else:
                 print('Can only process adding things with 1-3 slots. Code changes needed for anything else.')
         if topic == 'hermes/intent/jimconner:delete':
-            self.log.info("Delete a thing")
-            self.datastore.del_animation(payload['slots'][0]['value']['value'])
+            self.log.info("Delete")
+            if payload['slots'][0]['value']['value'] == 'everything':
+                self.datastore.strip_vals=[0,0,0,0]
+                self.datastore.animations=[]
+                self.datastore.filters=[]
+                self.datastore.strip_animations=[]
+                self.datastore.add_animation("set_strips")
+            else:
+                self.datastore.del_animation(payload['slots'][0]['value']['value'])
         if topic == 'hermes/intent/jimconner:brightness':
             if len(payload['slots']) == 1:
                 self.datastore.master_brightness=float(payload['slots'][0]['value']['value']/100)
